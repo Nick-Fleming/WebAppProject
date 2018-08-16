72674b7cde2c396f04c40c33271e8147039cf330
nickf115 <nickf115@192.168.0.108>
2018-08-15 21:11:24 -0400

    [DEV] initial push

---
+++ WebAppProject/src/main/java/com/nick/WebAppProject/App.java
@@ -0,0 +1,13 @@
+package com.nick.WebAppProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
